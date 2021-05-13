ded98366c7e8624a152b88b499aaa3d553c1ec6f
idhayamoorthy <72979714+idhayamoorthy@users.noreply.github.com>
2021-05-13 19:46:39 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,41 @@
+<?php
+
+$host = "localhost";
+$dbemail = "root";
+$dbpassword = "";
+$dbname = "telemedicine";
+
+$conn = new mysqli ($host, $dbemail, $dbpassword, $dbname);
+
+if (mysqli_connect_error()){
+  die('Connect Error ('. mysqli_connect_errno() .') '
+    . mysqli_connect_error());
+}
+
+session_start();
+
+if (isset($_POST['email'])){
+        // removes backslashes
+    $email = stripslashes($_REQUEST['email']);
+        //escapes special characters in a string
+ $email = mysqli_real_escape_string($conn,$email);
+ $password = stripslashes($_REQUEST['password']);
+ $password = mysqli_real_escape_string($conn,$password);
+ //Checking is user existing in the database or not
+ $query = "SELECT * FROM `register` WHERE email='$email'and password='$password'";
+ $result = mysqli_query($conn,$query) or die(mysql_error());
+ $rows = mysqli_num_rows($result);
+    if($rows==1)
+    {
+        $_SESSION['email'] = $email;
+            // Redirect user to index.php
+     header("Location:dashboard.php");
+ }
+    else{
+     echo "
+     <h1 style='text-align:center;'>E-mail or password is incorrect </h1>
+     <p style='text-align:center;'> Click here to go <a href='login.html'>Login page</a> </p>";
+         }
+}
+
+?>
